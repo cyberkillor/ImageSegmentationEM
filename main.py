81d4067a9bf619cb8bb49fbb45544331249836dd
cyberkillor <842693999@qq.com>
2021-06-20 13:23:55 +0800

    update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ memory_gpu = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
 print('Using GPU:' + str(np.argmax(memory_gpu)))
 os.environ["CUDA_VISIBLE_DEVICES"] = str(np.argmax(memory_gpu))
 os.system('rm tmp')
+# choose most available GPU
 import argparse
 from solver import Solver
 from data_loader import get_loader
@@ -16,9 +17,9 @@ import random
 
 def main(config):
     cudnn.benchmark = True
-    if config.model_type not in ['U_Net','R2U_Net','AttU_Net','R2AttU_Net']:
+    if config.model_type not in ['U_Net', 'R2U_Net', 'AttU_Net', 'R2AttU_Net']:
         print('ERROR!! model_type should be selected in U_Net/R2U_Net/AttU_Net/R2AttU_Net')
-        print('Your input for model_type was %s'%config.model_type)
+        print('Your input for model_type was %s' % config.model_type)
         return
 
     # Create directories if not exist
@@ -26,17 +27,15 @@ def main(config):
         os.makedirs(config.model_path)
     if not os.path.exists(config.result_path):
         os.makedirs(config.result_path)
-    config.result_path = os.path.join(config.result_path,config.model_type)
+    config.result_path = os.path.join(config.result_path, config.model_type)
     if not os.path.exists(config.result_path):
         os.makedirs(config.result_path)
-    
-    # lr = random.random()*0.0005 + 0.0000005
-    lr = 0.001
-    augmentation_prob= random.random()*0.7
-    # epoch = random.choice([100,150,200,250])
+
+    lr = config.lr
+    augmentation_prob = random.random() * 0.7
     epoch = config.epoch
-    decay_ratio = random.random()*0.8
-    decay_epoch = int(epoch*decay_ratio)
+    decay_ratio = random.random() * 0.8
+    decay_epoch = int(epoch * decay_ratio)
 
     config.augmentation_prob = augmentation_prob
     config.num_epochs = epoch
@@ -44,29 +43,28 @@ def main(config):
     config.num_epochs_decay = decay_epoch
 
     print(config)
-        
+
     train_loader = get_loader(image_path=config.train_path,
-                            image_size=config.image_size,
-                            batch_size=config.batch_size,
-                            num_workers=config.num_workers,
-                            mode='train',
-                            augmentation_prob=config.augmentation_prob)
+                              image_size=config.image_size,
+                              batch_size=config.batch_size,
+                              num_workers=config.num_workers,
+                              mode='train',
+                              augmentation_prob=config.augmentation_prob)
     valid_loader = get_loader(image_path=config.valid_path,
-                            image_size=config.image_size,
-                            batch_size=config.batch_size,
-                            num_workers=config.num_workers,
-                            mode='valid',
-                            augmentation_prob=0.)
+                              image_size=config.image_size,
+                              batch_size=config.batch_size,
+                              num_workers=config.num_workers,
+                              mode='valid',
+                              augmentation_prob=0.)
     test_loader = get_loader(image_path=config.test_path,
-                            image_size=config.image_size,
-                            batch_size=config.batch_size,
-                            num_workers=config.num_workers,
-                            mode='test',
-                            augmentation_prob=0.)
+                             image_size=config.image_size,
+                             batch_size=config.batch_size,
+                             num_workers=config.num_workers,
+                             mode='test',
+                             augmentation_prob=0.)
 
     solver = Solver(config, train_loader, valid_loader, test_loader)
 
-    
     # Train and sample the images
     if config.mode == 'train':
         solver.train()
@@ -77,11 +75,10 @@ def main(config):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    
     # model hyper-parameters
     parser.add_argument('--image_size', type=int, default=512)
     parser.add_argument('--t', type=int, default=3, help='t for Recurrent step of R2U_Net or R2AttU_Net')
-    
+
     # training hyper-parameters
     parser.add_argument('--img_ch', type=int, default=1)
     parser.add_argument('--output_ch', type=int, default=1)
@@ -90,8 +87,8 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--num_workers', type=int, default=1)
     parser.add_argument('--lr', type=float, default=0.0002)
-    parser.add_argument('--beta1', type=float, default=0.5)        # momentum1 in Adam
-    parser.add_argument('--beta2', type=float, default=0.999)      # momentum2 in Adam    
+    parser.add_argument('--beta1', type=float, default=0.5)  # momentum1 in Adam
+    parser.add_argument('--beta2', type=float, default=0.999)  # momentum2 in Adam
     parser.add_argument('--augmentation_prob', type=float, default=0.4)
 
     parser.add_argument('--log_step', type=int, default=2)
@@ -108,5 +105,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--cuda_idx', type=int, default=1)
     parser.add_argument('--epoch', type=int, default=300)
+    parser.add_argument('--lr', types=float, default=0.001)
     config = parser.parse_args()
     main(config)
